d8159d8fc4cf3333256816e398e017313dcc46c5
tbarblan <tobias.barblan@epfl.ch>
2017-11-17 20:24:59 +0000

    aaa

--- main.c
+++ main.c
@@ -6,10 +6,18 @@ int main(int argc, char * argv[]){
 
     network *n;
     if(argc >= 1){
+    if(argc > 1){
     
         n = createNetwork(argv[1]);
         //printNetwork(getNetwork(n));
+        showStats(n);
+<<<<<<< HEAD
+    }
+    else {
+        printf("No argument passed\n");
+=======
+>>>>>>> 195a9242aba484dba7f1214be2a3a61c42e7a856
     }
 
     exit(0);
-}
+}}
